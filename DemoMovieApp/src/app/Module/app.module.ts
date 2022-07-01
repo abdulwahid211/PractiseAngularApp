4a6e67a26b4e156e074f834510c148a9f33a295d
Abdul <abdulwahid211@gmail.com>
2022-07-01 01:55:56 +0100

    removed excess files

--- DemoMovieApp/src/app/Module/app.module.ts
+++ DemoMovieApp/src/app/Module/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from '../Components/app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { WelcomeComponent } from 'src/app/home/welcome.component';
 import { MovieComponent } from '../Components/Movie/movie.component';
 import { MovieListComponent } from '../Components/Movie/movies-list.component';
 import { MovieProfileComponent } from '../Components/Movie/movie-profile.component';
 import { MovieDetailGuard } from '../Components/Movie/movies-detail.guard';
-import { SuitsComponent } from '../home/suits';
 
 @NgModule({
   declarations: [
@@ -18,7 +16,6 @@ import { SuitsComponent } from '../home/suits';
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'welcome', component: WelcomeComponent },
       { path: 'movies', component: MovieListComponent },
       {
         path: 'movie/:id',
--- DemoMovieApp/src/app/home/suits.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './welcome.component.html'
-})
-export class SuitsComponent {
-  public pageTitle = 'Suits!!!';
-}
